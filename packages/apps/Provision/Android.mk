428755d390653870eecdaf7ee472e72b00b9a9e5
Young-Ho Cha <ganadist@gmail.com>
2015-10-30 12:25:57 +0900

    occam: apply odex in provision
    Signed-off-by: Young-Ho Cha <ganadist@gmail.com>

--- packages/apps/Provision/Android.mk
+++ packages/apps/Provision/Android.mk
@@ -7,7 +7,6 @@ LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_PACKAGE_NAME := FakeNexusProvision
 LOCAL_CERTIFICATE := platform
-LOCAL_DEX_PREOPT := false
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
